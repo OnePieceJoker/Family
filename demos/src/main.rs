9cb7ac41d0fd3136a1521134bcc557b3ad9cb561
zhihong <joker_lizhih@163.com>
2023-04-14 10:32:27 +0800

    first commit

---
+++ demos/src/main.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Lizhihong".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+} 
+
